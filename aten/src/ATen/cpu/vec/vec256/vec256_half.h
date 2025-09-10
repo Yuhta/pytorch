ff965f41afdb11611bdcfa7f952a31d829393861
Nicolas De Carli <ndecarli@meta.com>
2025-09-10 13:22:25 -0400

    Update vec256_half.h

--- aten/src/ATen/cpu/vec/vec256/vec256_half.h
+++ aten/src/ATen/cpu/vec/vec256/vec256_half.h
@@ -268,7 +268,7 @@ LOAD_FP32_VECTORIZED_INIT(Half, fp16)
 
 #else // defined(CPU_CAPABILITY_AVX2)
 
-#if !defined(__aarch64__)
+#if !defined(__aarch64__) || defined(CPU_CAPABILITY_SVE256)
 CONVERT_NON_VECTORIZED_INIT(Half, half)
 #endif
 
