490f95ad198087fbf1ac40d8db92c37111569f76
Nicolas De Carli <ndecarli@meta.com>
2025-09-11 16:23:57 -0400

    Update cpu_vec_isa.py

--- torch/_inductor/cpu_vec_isa.py
+++ torch/_inductor/cpu_vec_isa.py
@@ -198,12 +198,14 @@ class VecSVE256(VecISA):
         "AT_BUILD_ARM_VECSVE_WITH_SLEEF",
         "__ARM_FEATURE_BF16",
     ]
-    _arch_flags = "-march=armv8-a+sve+bf16 -msve-vector-bits=256"
+    _arch_flags = "-march=armv8.2-a+sve+bf16 -msve-vector-bits=256"
 
     _dtype_nelements = {torch.float: 8, torch.bfloat16: 16, torch.float16: 16}
 
     def __str__(self) -> str:
-        return "sve256"
+        if config.is_fbcode():
+            return "sve256"
+        return "asimd"
 
     __hash__: Callable[[VecISA], Any] = VecISA.__hash__
 
